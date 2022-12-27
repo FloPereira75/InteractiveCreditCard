cdb30f1af1cf2bce9aa1847c8e2d3d36709830e1
Flo_75 <flopereira75@gmail.com>
2022-12-27 21:18:40 +0100

    form validator in javascript done - project done

--- js/main.js
+++ js/main.js
@@ -8,35 +8,64 @@ const inputCardNumber = document.getElementById("input-card-number");
 const inputCardMonth = document.getElementById("input-card-month");
 const inputCardYear = document.getElementById("input-card-year");
 const inputCardCvc = document.getElementById("input-card-cvc");
-
 const formDiv = document.getElementById("form");
 const resultDiv = document.getElementById("result");
 const subBtn = document.getElementById("sub-btn");
+const blankError = document.querySelectorAll(".blank-error");
+const formatError = document.querySelectorAll(".format-error");
 
-inputCardName.addEventListener("input", (e) => {
-  console.log(e.target.value);
-  cardName.textContent = e.target.value;
-});
+function setCardName(e) {
+  cardName.innerText = format(e.target.value);
+}
 
-inputCardNumber.addEventListener("input", (e) => {
-  cardNumber.textContent = e.target.value;
-});
+function setCardNumber(e) {
+  cardNumber.innerText = format(e.target.value);
+}
 
-inputCardMonth.addEventListener("input", (e) => {
-  cardMonth.textContent = e.target.value;
-});
+function setCardMonth(e) {
+  cardMonth.innerText = format(e.target.value);
+}
 
-inputCardYear.addEventListener("input", (e) => {
-  cardYear.textContent = e.target.value;
-});
+function setCardYear(e) {
+  cardYear.innerText = format(e.target.value);
+}
 
-inputCardCvc.addEventListener("input", (e) => {
-  cardCvc.textContent = e.target.value;
-});
+function setCardCvc(e) {
+  cardCvc.innerText = format(e.target.value);
+}
 
-formDiv.addEventListener("submit", (e) => {
-  console.log("OK OK");
+function handleSubmit(e) {
   e.preventDefault();
-  formDiv.style.display = "none";
-  resultDiv.style.display = "flex";
-});
+  if (!inputCardName.value) {
+    blankError.style.display = "block";
+  } else if (!inputCardNumber.value || inputCardNumber.value.length < 16) {
+    blankError.style.display = "block";
+  } else if (!inputCardMonth.value) {
+    blankError.style.display = "block";
+  } else if (!inputCardYear.value) {
+    blankError.style.display = "block";
+  } else if (!inputCardCvc.value) {
+    blankError.style.display = "block";
+  } else if (
+    inputCardName.value &&
+    inputCardNumber.value &&
+    inputCardMonth.value &&
+    inputCardYear.value &&
+    inputCardCvc.value &&
+    inputCardNumber.value.length == 16
+  ) {
+    formDiv.style.display = "none";
+    resultDiv.style.display = "flex";
+  }
+}
+
+function format(s) {
+  return s.toString().replace(/\d{4}(?=.)/g, "$& ");
+}
+
+inputCardName.addEventListener("input", setCardName);
+inputCardNumber.addEventListener("input", setCardNumber);
+inputCardMonth.addEventListener("input", setCardMonth);
+inputCardYear.addEventListener("input", setCardYear);
+inputCardCvc.addEventListener("input", setCardCvc);
+formDiv.addEventListener("submit", handleSubmit);
